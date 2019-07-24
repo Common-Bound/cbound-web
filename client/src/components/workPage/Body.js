2e06958219b42c821b6cc04da6ebdbca3208c77d
HyunSeoChoi <arenaofjagal@naver.com>
2019-07-24 16:17:31 +0900

    작업 처리한 내용 서버로 전송

--- client/src/components/workPage/Body.js
+++ client/src/components/workPage/Body.js
@@ -1,59 +1,19 @@
 import React, { Component } from "react";
-import CropperJS from "react-cropper";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import "./Body.css";
+import CropInfoList from "./CropInfoList.js";
 
 class Body extends Component {
-  _crop() {
-    // image in dataUrl
-    console.log(this.refs.cropper.getCroppedCanvas().toDataURL());
-  }
-
-  render() {
-    return (
-      <CropperJS
-        ref="cropper"
-        src="http://i.imgur.com/n483ZwJ.jpg"
-        style={{ height: 400, width: "100%" }}
-        // Cropper.js options
-        aspectRatio={16 / 9}
-        guides={false}
-        crop={this._crop.bind(this)}
-      />
-    );
-  } /*
   state = {
-    orig_image: "a",
-    crop_image: []
+    orig_image: "",
+    crop_image: [],
+    crop: {},
+    label: ""
   };
 
-  onFileSelected = async e => {
-    await this.setState({
-      orig_image: URL.createObjectURL(e.target.files[0])
-    });
-  };
-
-  fetchTest() {
+  sendData = bodyData => {
     console.log("submitted");
-    var bodyData = {
-      orig_image: "url",
-      meta: {
-        crop_image: [
-          {
-            x: 0,
-            y: 0,
-            width: 0,
-            height: 0,
-            label: "123"
-          },
-          {
-            x: 2,
-            y: 4,
-            width: 6,
-            height: 1,
-            label: "3214"
-          }
-        ]
-      }
-    };
 
     fetch("/task", {
       method: "post",
@@ -71,45 +31,127 @@ class Body extends Component {
       .catch(function(ex) {
         console.log("error occured" + ex);
       });
+  };
+
+  onFileSelected = async e => {
+    // 파일 보내주면 됨
+    await this.setState(
+      {
+        orig_image: URL.createObjectURL(e.target.files[0])
+      },
+      () => {
+        this.sendData({ orig_image: this.state.orig_image });
+      }
+    );
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleImageLoaded = image => {
+    console.log(image);
+  };
+
+  handleOnCropChange = crop => {
+    this.setState({ crop: crop });
+  };
+
+  handleOnCropComplete = e => {
+    const cropData = this.state.crop;
+    console.log(cropData);
+    if (this.state.orig_image && this.state.label) {
+      this.setState({
+        crop_image: this.state.crop_image.concat({
+          x: cropData.x,
+          y: cropData.y,
+          width: cropData.width,
+          height: cropData.height,
+          label: this.state.label
+        }),
+        label: ""
+      });
+    }
+  };
+
+  handleSendAll() {
+    this.sendData({
+      orig_image: this.state.orig_image,
+      meta: {
+        crop_image: this.state.crop_image
+      }
+    });
   }
+
+  handleKeyPress = e => {
+    if (e.charCode === 13) {
+      e.preventDefault();
+      this.handleOnCropComplete();
+    }
+  };
   render() {
     const workStyle = {
       borderTop: "3px solid lightgrey"
     };
-
     return (
       <div>
         <hr style={workStyle} />
         <div>
-          <img
-            id="workImg"
+          <ReactCrop
             src={this.state.orig_image}
-            alt="workImg"
-            style={{ maxWidth: "100%" }}
+            crop={this.state.crop}
+            onChange={this.handleOnCropChange}
+            onImageLoaded={this.handleImageLoaded}
           />
         </div>
         <br />
 
         <div>
           <form>
-            <input type="file" onChange={this.onFileSelected} />
-            <div>
+            <label className="fileContainer">
+              업로드
+              <input type="file" onChange={this.onFileSelected} />
+            </label>
+            <div className="input-group mb-3">
               <input
-                type="button"
-                className="btn btn-outline-secondary"
-                value="링크"
+                value={this.state.label}
+                className="form-control"
+                onChange={this.handleChange}
+                onKeyPress={this.handleKeyPress}
+                name="label"
+                placeholder="label"
+                aria-label="label"
+                aria-describedby="label input"
               />
-              <input
-                type="button"
-                className="btn btn-outline-secondary"
-                value="저장"
-              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-outline-secondary"
+                  type="button"
+                  onClick={this.handleOnCropComplete}
+                  id="button-addon2"
+                >
+                  저장
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  class="btn btn-outline-secondary"
+                  onClick={this.handleSendAll}
+                >
+                  완전 다 보내기
+                </button>
+              </div>
             </div>
           </form>
         </div>
+
+        {JSON.stringify(this.state.crop_image)}
       </div>
     );
-  }*/
+  }
 }
 
 export default Body;
---
+++ client/src/components/workPage/CropInfoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class CropInfoList extends Component {
+  static defaultProps = {
+    data: []
+  };
+
+  render() {
+    const { data } = this.props;
+    //const list = data.map(info => <PhoneInfo key={info.id} info={info} />);
+
+    return null;
+  }
+}
+
+export default CropInfoList;
