6c783e87ef1bddea13650c0490d9fe3ffbdf1095
eunsukimme <eunsu.dev@gmail.com>
2019-08-28 01:10:33 +0900

    쉘 스크립트 오타 수정

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -4,7 +4,7 @@ set -f
 string=$DEPLOY_SERVER
 array=(${string//,/ })
 #Iterate servers for deploy and pull last commit
-for i in "${!array[@]}"do    
+for i in "${!array[@]}"; do    
       echo "Deploy project on server ${array[i]}"    
       ssh ubuntu@${array[i]} "cd /DALA && git pull origin master"
 done
