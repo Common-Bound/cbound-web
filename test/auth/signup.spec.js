781f02bc2c7c36a203507f5634536f6f37fb952b
eunsukimme <eunsu.dev@gmail.com>
2019-08-28 00:25:42 +0900

    이미 존재하는 사용자로 회원가입 시도하는 테스트 코드 작성 및 테스트 완료

--- test/auth/signup.spec.js
+++ test/auth/signup.spec.js
@@ -32,4 +32,24 @@ describe("# 생산자 회원가입 테스트", function() {
       });
     });
   });
+
+  it("존재하는 아이디로 회원가입 테스트", function(done) {
+    // 기존에 존재하는 테스트 계정(email: 1, pw: 1)으로 회원가입 시도
+    const id = uuid();
+    const email = 1;
+    const pw = 1;
+    bcrypt.hash(pw, null, null, (err, hash) => {
+      const sql = `insert into data_creator values($1, $2, $3)`;
+      db.query(sql, [id, email, pw], (err, results) => {
+        // db 에러시 테스트 성공(이미 존재하는 계정인데 insert 시도하므로)
+        if (err) {
+          done();
+        }
+        // 이미 존재하는 계정인데 회원가입이 되면 실패!
+        else {
+          done(false);
+        }
+      });
+    });
+  });
 });
