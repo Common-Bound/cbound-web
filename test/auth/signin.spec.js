e4b2d33ff190cbdd5c178e749db42f3cf2569cc0
eunsukimme <eunsu.dev@gmail.com>
2019-08-27 18:07:34 +0900

    테스트 기능 및 Gitlab CI yml 파일 추가
    테스트코드를 작성하는 test 폴더 추가함. 테스트 폴더에서 auth 라는 폴더 내 로그인 기능을 테스트하는 코드 추가하고 .gitlab-ci.yml 파일 생성해서 CI 파이프라인을 돌게 함

---
+++ test/auth/signin.spec.js
@@ -0,0 +1,72 @@
+const assert = require("assert");
+const db = require("../../db/index");
+const bcrypt = require("bcrypt-nodejs");
+const uuid = require("uuid/v4");
+
+describe("# 로그인 테스트", function() {
+  it("사용자 로그인 테스트", function(done) {
+    // 테스트용 계정(email: 1, password: 1)로 로드인 시도
+    const id = 1;
+    const pw = 1;
+    bcrypt.hash(pw, null, null, function(err, hash) {
+      const sql = `SELECT * FROM data_creator WHERE email='${id}' and password='${hash}'`;
+      db.query(sql, [], function(err, results) {
+        // db 에러시 실패
+        if (err) {
+          done(err);
+        }
+        // 사용자가 존재하지 않으면 실패. 반드시 로그인 성공해야함
+        else if (results.rows.length < 1) {
+          done(false);
+        }
+        // 사용자가 존재하면 성공
+        else {
+          done();
+        }
+      });
+    });
+  });
+
+  it("사용자 비밀번호 오류 테스트", function(done) {
+    // 테스트용 계정 (email: 1)로 비밀번호를 다르게해서 로그인 시도
+    const id = 1;
+    const pw = uuid();
+    bcrypt.hash(pw, null, null, function(err, hash) {
+      const sql = `SELECT * FROM data_creator WHERE email='${id}' and password='${hash}'`;
+      db.query(sql, [], function(err, results) {
+        // db 에러시 실패
+        if (err) {
+          done(err);
+        }
+        // 사용자가 존재하면 실패. 비밀번호 오류 반환해야 성공
+        else if (results.rows.length > 0) {
+          done(false);
+        }
+        // 사용자 존재하지 않으면 성공
+        else {
+          done();
+        }
+      });
+    });
+  });
+
+  it("존재하지 않는 사용자 로그인 테스트", function(done) {
+    // 랜덤하게 생성된 uuid ID/PW 로 로그인 시도
+    const id = uuid();
+    const sql = `SELECT * FROM data_creator WHERE email='${id}'`;
+    db.query(sql, [], function(err, results) {
+      // db 에러시 실패
+      if (err) {
+        done(err);
+      }
+      // 사용자가 존재하면 실패. 반드시 존재하지 않는 사용자 이여야 함
+      else if (results.rows.length > 0) {
+        done(false);
+      }
+      // 사용자가 존재하지 않으면 성공
+      else {
+        done();
+      }
+    });
+  });
+});
