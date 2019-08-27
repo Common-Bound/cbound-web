eff849d440067725200b420f90f70f98a0352337
eunsukimme <eunsu.dev@gmail.com>
2019-08-27 20:57:12 +0900

    회원가입 테스트 코드 작성

---
+++ test/auth/signup.spec.js
@@ -0,0 +1,7 @@
+const db = require("../../db/index");
+const bcrypt = require("bcrypt-nodejs");
+const uuid = require("uuid/v4");
+
+describe("# 회원가입 테스트", function() {
+  it("사용자 회원가입 테스트", function(done) {});
+});
