d1dc5a3e47e4919857c216e051bf3a32a5dfafa3
eunsukimme <eunsu.dev@gmail.com>
2019-08-30 16:34:26 +0900

    Revert ".gitlab-ci.yml 수정"
    This reverts commit 5c58750ddf19218af9264404ea4d2f81e4c2d635.

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -8,6 +8,5 @@ for i in "${!array[@]}"; do
       echo "Deploy project on server ${array[i]}"    
       ssh ubuntu@${array[i]} "cd cbound &&
       git pull origin master &&
-      yarn build &&
       pm2 start ecosystem.config.js"
 done
