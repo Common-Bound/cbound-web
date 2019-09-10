50325ea53d0964ec137fc625ef06f1f42d93e629
eunsukimme <eunsu.dev@gmail.com>
2019-09-10 17:51:31 +0900

    Workspace 페이지 아이콘 안보이는 버그 수정

--- client/src/components/mypage/CreatorBody.js
+++ client/src/components/mypage/CreatorBody.js
@@ -56,7 +56,7 @@ const MyProjectButton = styled(StyledLink)`
 
 const TaskButton = styled(StyledLink)`
   color: ${props =>
-    props.pathname.includes('/mypage/creator/task')
+    props.pathname.includes("/mypage/creator/task")
       ? "white"
       : "rgba(255, 255, 255, 0.3)"};
 `;
@@ -101,7 +101,7 @@ class CreatorBody extends Component {
             to={`${this.props.match.url}/task/standalone/none`}
             pathname={this.props.location.pathname}
           >
-            <Icon class="fas fa-crop-alt"></Icon>
+            <Icon className="fas fa-crop-alt"></Icon>
             <IconTitle>Workspace</IconTitle>
           </TaskButton>
           <StyledLink
