9e22b7d94a7d3ba77d291bcf37e4ad429d980400
eunsukimme <eunsu.dev@gmail.com>
2019-10-09 04:11:20 +0900

    Gitlab CI deploy 테스트 커밋

--- server.js
+++ server.js
@@ -59,6 +59,9 @@ app.use("/api", apiRouter);
 app.use("/cmd", cmdRouter);
 
 app.get("/*", (req, res, next) => {
+  console.log(
+    "만약 이 코드가 Gitlab CI deploy 이후 반영이 되어있다면 이는 제대로 배포가 되었음을 의미한다."
+  );
   res.sendFile(path.join(__dirname, "client/build/index.html"), function(err) {
     if (err) {
       logger.error(err);
