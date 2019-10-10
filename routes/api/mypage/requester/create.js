1a0040417e8c6d84f6d40920312a5f93d054ff01
eunsukimme <eunsu.dev@gmail.com>
2019-10-10 21:51:48 +0900

    데이터를 Form 으로 받아오도록 로직 수정

--- routes/api/mypage/requester/create.js
+++ routes/api/mypage/requester/create.js
@@ -10,7 +10,7 @@ const multer = require("multer");
 const multerS3 = require("multer-s3");
 
 // AWS config 파일 불러오기
-AWS.config.loadFromPath(__dirname + "/../../../../../../config/awsConfig.json");
+AWS.config.loadFromPath(__dirname + "/../../../../config/awsConfig.json");
 let s3 = new AWS.S3();
 
 // S3에 올리는 모듈
@@ -38,93 +38,99 @@ const fileLogger = (req, res, next) => {
 };
 
 // path: ~/api/mypage/requester/create
-router.post("/", upload_s3("title_image"), fileLogger, (req, res, next) => {
-  console.log(req.body);
-  if (!req.user) {
-    return res.status(401).json({ message: "로그인 해주세요" });
-  }
-  const normal_id = uuid();
-  const inspection_id = uuid();
-  const user_id = req.user.id;
-  const title_image = req.file.location;
-
-  var created_at = moment()
-    .tz("Asia/Seoul")
-    .format();
-  var due_date = moment(req.body.due_date)
-    .tz("Asia/Seoul")
-    .format();
-  // id, ref_project, title, title_image, simple_description, detail_description,
-  // due_date, created_at, type, project_type, guideline_url, reward
-
-  db.query(
-    "insert into project values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
-    [
-      normal_id,
-      null,
-      req.body.title,
-      title_image,
-      req.body.simple_description,
-      req.body.detail_description,
-      due_date,
-      created_at,
-      req.body.type,
-      "normal",
-      req.body.guideline_url,
-      req.body.reward
-    ],
-    (err, result) => {
-      if (err) {
-        logger.error(err);
-        return res.status(500).send(err);
-      }
-      // 검수 프로젝트 추가
-      db.query(
-        "insert into project values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
-        [
-          inspection_id,
-          normal_id,
-          req.body.title,
-          req.body.title_image,
-          req.body.simple_description,
-          req.body.detail_description,
-          due_date,
-          created_at,
-          req.body.type,
-          "inspection",
-          req.body.guideline_url,
-          req.body.reward
-        ],
-        (err, result) => {
-          if (err) {
-            logger.error(err);
-            return res.status(500).send(err);
-          }
-          db.query(
-            "insert into requester_pool values($1, $2)",
-            [normal_id, user_id],
-            (err, result) => {
-              if (err) {
-                logger.error(err);
-                return res.status(500).send(err);
-              }
-              db.query(
-                "insert into requester_pool values($1, $2)",
-                [inspection_id, user_id],
-                (err, result) => {
-                  if (err) {
-                    logger.error(err);
-                    return res.status(500).send(err);
-                  }
-                  return res.json({ result: true });
-                }
-              );
-            }
-          );
-        }
-      );
+router.post(
+  "/",
+  upload_s3.single("title_image"),
+  fileLogger,
+  (req, res, next) => {
+    console.log(req.body);
+    if (!req.user) {
+      return res.status(401).json({ message: "로그인 해주세요" });
     }
-  );
-});
+    const normal_id = uuid();
+    const inspection_id = uuid();
+    const info = req.info;
+    const user_id = info.user.id;
+    const title_image = req.file.location;
+
+    var created_at = moment()
+      .tz("Asia/Seoul")
+      .format();
+    var due_date = moment(info.due_date)
+      .tz("Asia/Seoul")
+      .format();
+    // id, ref_project, title, title_image, simple_description, detail_description,
+    // due_date, created_at, type, project_type, guideline_url, reward
+
+    db.query(
+      "insert into project values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
+      [
+        normal_id,
+        null,
+        info.title,
+        title_image,
+        info.simple_description,
+        info.detail_description,
+        due_date,
+        created_at,
+        info.type,
+        "normal",
+        info.guideline_url,
+        info.reward
+      ],
+      (err, result) => {
+        if (err) {
+          logger.error(err);
+          return res.status(500).send(err);
+        }
+        // 검수 프로젝트 추가
+        db.query(
+          "insert into project values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
+          [
+            inspection_id,
+            normal_id,
+            info.title,
+            info.title_image,
+            info.simple_description,
+            info.detail_description,
+            due_date,
+            created_at,
+            info.type,
+            "inspection",
+            info.guideline_url,
+            info.reward
+          ],
+          (err, result) => {
+            if (err) {
+              logger.error(err);
+              return res.status(500).send(err);
+            }
+            db.query(
+              "insert into requester_pool values($1, $2)",
+              [normal_id, user_id],
+              (err, result) => {
+                if (err) {
+                  logger.error(err);
+                  return res.status(500).send(err);
+                }
+                db.query(
+                  "insert into requester_pool values($1, $2)",
+                  [inspection_id, user_id],
+                  (err, result) => {
+                    if (err) {
+                      logger.error(err);
+                      return res.status(500).send(err);
+                    }
+                    return res.json({ result: true });
+                  }
+                );
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+);
 
 module.exports = router;
